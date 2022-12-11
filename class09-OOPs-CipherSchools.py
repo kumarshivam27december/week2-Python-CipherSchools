94e0940df1490fcaa07df7ed77648ccb289c3147
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-11 12:41:51 +0530

    version 3.0

---
+++ class09-OOPs-CipherSchools.py
@@ -0,0 +1,55 @@
+stud1={
+    
+    "name": "shivam kumar",
+    "age": 119
+    
+}
+
+stud2={
+    "name":"loveu singh",
+    "age": 117
+}
+
+
+print()
+
+
+
+class Person:
+    pass
+p=Person()
+print(p)
+
+print(hex(id(p)))
+
+
+
+a=1000000
+def square(a):
+    print(a**2)
+    
+
+
+
+
+class person:
+    def say_hi(self):
+        self.name="whats my name"
+        print('Hello,how are you ' + self.name + "?")
+
+p=person()
+p.say_hi()    
+
+
+
+
+class person:
+    name="jai bihar"
+    def say_hi(self):
+        print(f"Hello everyone : I am {self.name}")
+        
+p=person()
+p.say_hi()
+
+person.say_hi(p)
+
