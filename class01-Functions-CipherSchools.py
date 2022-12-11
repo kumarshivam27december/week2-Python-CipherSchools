22caf4e623d98cdf8b317b00f1d530cdcd51a789
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-11 10:42:39 +0530

    version 2.0

---
+++ class01-Functions-CipherSchools.py
@@ -0,0 +1,115 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+
+
+
+
+a=5
+b=7
+print(a+b)
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+
+print(a-b)
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+
+print(a*b)
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+print('loading.....')
+
+
+
+x=6
+x="lovely professional university"
+x=1.5
+x=print
+
+def showloadind():
+  for i in range(3):
+    print("loading.....","."*())
+    
+    
+    
+
+def sheldon_knock(name):
+  for i in range(3):
+    print("knock knock knock ",name)
+sheldon_knock("leopard")
+
+
+
+
+def sheldon_knock(name,time=25):
+  for i in range(time):
+    print("knock knock knock ",name)
+sheldon_knock("i love you")
+
+
+def sheldon_knock(name,time=25):
+  for i in range(time):
+    print("knock knock knock ",name)
+sheldon_knock("i love you",100)
+
+
+
+
+
